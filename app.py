bbcd6072de83a286e045a0b830beb55894034717
regi <63766962+reiyiyi@users.noreply.github.com>
2021-03-29 19:19:01 +0900

    ページごとに分けるためにapp.pyを編集

--- app.py
+++ app.py
@@ -27,6 +27,139 @@ def ranking():
         time_data = json.load(f2)
 
     return render_template('ranking.html',
+                        ranking_type="全体",
+                        hot_problems_data=hot_problems_data,
+                        time_data=time_data)
+
+@app.route('/ranking-ac0', methods=["GET"])
+def ranking_ac0():
+    problems_file_name, time_file_name = filename.get()
+    problems_file_name = problems_file_name + "0"
+
+    s3 = aws.get()
+
+    bucket = s3.Bucket('hotproblems')
+    bucket.download_file('hot_problems_data/' + problems_file_name + '.json', problems_file_name + '.json')
+    bucket.download_file('hot_problems_data/' + time_file_name, time_file_name)
+
+    with open(problems_file_name + '.json', encoding='utf-8') as f:
+        hot_problems_data = json.load(f)
+
+    with open(time_file_name, encoding='utf-8') as f2:
+        time_data = json.load(f2)
+
+    return render_template('ranking.html',
+                        ranking_type="AC数0~199",
+                        hot_problems_data=hot_problems_data,
+                        time_data=time_data)
+
+@app.route('/ranking-ac200', methods=["GET"])
+def ranking_ac200():
+    problems_file_name, time_file_name = filename.get()
+    problems_file_name = problems_file_name + "200"
+
+    s3 = aws.get()
+
+    bucket = s3.Bucket('hotproblems')
+    bucket.download_file('hot_problems_data/' + problems_file_name + '.json', problems_file_name + '.json')
+    bucket.download_file('hot_problems_data/' + time_file_name, time_file_name)
+
+    with open(problems_file_name + '.json', encoding='utf-8') as f:
+        hot_problems_data = json.load(f)
+
+    with open(time_file_name, encoding='utf-8') as f2:
+        time_data = json.load(f2)
+
+    return render_template('ranking.html',
+                        ranking_type="AC数200~399",
+                        hot_problems_data=hot_problems_data,
+                        time_data=time_data)
+
+@app.route('/ranking-ac400', methods=["GET"])
+def ranking_ac400():
+    problems_file_name, time_file_name = filename.get()
+    problems_file_name = problems_file_name + "400"
+
+    s3 = aws.get()
+
+    bucket = s3.Bucket('hotproblems')
+    bucket.download_file('hot_problems_data/' + problems_file_name + '.json', problems_file_name + '.json')
+    bucket.download_file('hot_problems_data/' + time_file_name, time_file_name)
+
+    with open(problems_file_name + '.json', encoding='utf-8') as f:
+        hot_problems_data = json.load(f)
+
+    with open(time_file_name, encoding='utf-8') as f2:
+        time_data = json.load(f2)
+
+    return render_template('ranking.html',
+                        ranking_type="AC数400~599",
+                        hot_problems_data=hot_problems_data,
+                        time_data=time_data)
+
+@app.route('/ranking-ac600', methods=["GET"])
+def ranking_ac600():
+    problems_file_name, time_file_name = filename.get()
+    problems_file_name = problems_file_name + "600"
+
+    s3 = aws.get()
+
+    bucket = s3.Bucket('hotproblems')
+    bucket.download_file('hot_problems_data/' + problems_file_name + '.json', problems_file_name + '.json')
+    bucket.download_file('hot_problems_data/' + time_file_name, time_file_name)
+
+    with open(problems_file_name + '.json', encoding='utf-8') as f:
+        hot_problems_data = json.load(f)
+
+    with open(time_file_name, encoding='utf-8') as f2:
+        time_data = json.load(f2)
+
+    return render_template('ranking.html',
+                        ranking_type="AC数600~799",
+                        hot_problems_data=hot_problems_data,
+                        time_data=time_data)
+
+@app.route('/ranking-ac800', methods=["GET"])
+def ranking_ac800():
+    problems_file_name, time_file_name = filename.get()
+    problems_file_name = problems_file_name + "800"
+
+    s3 = aws.get()
+
+    bucket = s3.Bucket('hotproblems')
+    bucket.download_file('hot_problems_data/' + problems_file_name + '.json', problems_file_name + '.json')
+    bucket.download_file('hot_problems_data/' + time_file_name, time_file_name)
+
+    with open(problems_file_name + '.json', encoding='utf-8') as f:
+        hot_problems_data = json.load(f)
+
+    with open(time_file_name, encoding='utf-8') as f2:
+        time_data = json.load(f2)
+
+    return render_template('ranking.html',
+                        ranking_type="AC数800~999",
+                        hot_problems_data=hot_problems_data,
+                        time_data=time_data)
+
+@app.route('/ranking-ac1000', methods=["GET"])
+def ranking_ac1000():
+    problems_file_name, time_file_name = filename.get()
+    problems_file_name = problems_file_name + "1000"
+
+    s3 = aws.get()
+
+    bucket = s3.Bucket('hotproblems')
+    bucket.download_file('hot_problems_data/' + problems_file_name + '.json', problems_file_name + '.json')
+    bucket.download_file('hot_problems_data/' + time_file_name, time_file_name)
+
+    with open(problems_file_name + '.json', encoding='utf-8') as f:
+        hot_problems_data = json.load(f)
+
+    with open(time_file_name, encoding='utf-8') as f2:
+        time_data = json.load(f2)
+
+    return render_template('ranking.html',
+                        ranking_type="AC数1000~",
                         hot_problems_data=hot_problems_data,
                         time_data=time_data)
 
